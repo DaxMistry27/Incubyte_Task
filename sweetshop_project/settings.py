3364bf00c8367cc99d540bd4efd7ed9fd39f2098
Dax Mistry <daxmistry83@gmail.com>
2025-07-16 17:18:53 +0530

    updated requirements.txt

--- sweetshop_project/settings.py
+++ sweetshop_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4sv+_rnt8&7egv6^s---doq=2b0snsxtuebn&)^kfyxo4qawe%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
